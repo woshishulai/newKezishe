65111e4c124a179a4422210d70ec76881893553a
lai shu <1811399887@qq.com>
2024-01-02 10:01:21 +0800

    add image

--- react-ts-project/src/components/Layout/BaseFooter/index.tsx
+++ react-ts-project/src/components/Layout/BaseFooter/index.tsx
@@ -1,9 +1,9 @@
-import style from "./index.module.less"
+import style from './index.module.less';
 function BaseFooter() {
     console.log('我是自定义的底部');
     return (
         <div className={style.footer}>
-            登录页面的
+            <img src={''} alt="" />
         </div>
     );
 }
--- react-ts-project/src/components/Layout/BaseHeader/index.tsx
+++ react-ts-project/src/components/Layout/BaseHeader/index.tsx
@@ -1,12 +1,8 @@
-import style from "./index.module.less"
+import style from './index.module.less';
 function BaseHeader() {
     console.log('我是自定义的头部');
 
-    return (
-        <div className={style.header}>
-            登录页面的头部
-        </div>
-    );
+    return <div className={style.header}></div>;
 }
 
 export default BaseHeader;
